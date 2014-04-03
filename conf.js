506be43ba260e07966e0358608af628a1fceb027
unknown <romain.braun@PARNUWKS01-152.nurun.com>
2014-04-03 16:57:33 +0200

    text is sliding, eternal loop

--- conf.js
+++ conf.js
@@ -1,5 +1,29 @@
 var settings = {
-	text : {
+	text : [
+		'Lorem ipsum dolor sit amet',
+		'consectetur adipiscing elit',
+		'Nullam adipiscing porta metus quis dignissim',
+		' Etiam odio orci, aliquam ac pulvinar in, ullamcorper sed augue',
+		'In ut justo non odio convallis ultrices',
+		'Quisque quam nunc, elementum sit amet ultrices sed, consectetur in sem',
+		'Nam vulputate pellentesque',
+		'diam eget sagittis',
+		'Sed id blandit leo',
+		'Nam nec ipsum tincidunt, aliquam risus eu, tincidunt magna',
+		'Cras egestas sagittis neque, a venenatis est aliquet ut',
+		'Nam egestas viverra neque',
+		'Lorem ipsum dolor sit amet',
+		'consectetur adipiscing elit',
+		'Nullam adipiscing porta metus quis dignissim',
+		' Etiam odio orci, aliquam ac pulvinar in, ullamcorper sed augue',
+		'In ut justo non odio convallis ultrices',
+		'Quisque quam nunc, elementum sit amet ultrices sed, consectetur in sem',
+		'Nam vulputate pellentesque',
+		'diam eget sagittis',
+		'Sed id blandit leo',
+		'Nam nec ipsum tincidunt, aliquam risus eu, tincidunt magna',
+		'Cras egestas sagittis neque, a venenatis est aliquet ut',
+		'Nam egestas viverra neque',
 		'Lorem ipsum dolor sit amet',
 		'consectetur adipiscing elit',
 		'Nullam adipiscing porta metus quis dignissim',
@@ -12,5 +36,5 @@ var settings = {
 		'Nam nec ipsum tincidunt, aliquam risus eu, tincidunt magna',
 		'Cras egestas sagittis neque, a venenatis est aliquet ut',
 		'Nam egestas viverra neque'
-	}
+	]
 };
--- index.js
+++ index.js
@@ -2,34 +2,88 @@ var scene 			= new THREE.Scene(),
 	camera 			= new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000),
 	renderer 		= new THREE.WebGLRenderer(),
 	textMaterial 	= new THREE.MeshBasicMaterial( { color: 0xffffff, overdraw: true } ),
-	text3d 			= new THREE.TextGeometry( 'hello', {
-						size: 80,
-						height: 20,
-						curveSegments: 2,
-						font: "helvetiker"
-					}),
-	text 			= new THREE.Mesh( text3d, textMaterial ),
-	group 			= new THREE.Object3D();
+	group 			= new THREE.Object3D(),
+	mouseY			= 0,
+	mouseX			= 0;
 
+	var windowHalfX = window.innerWidth / 2;
+	var windowHalfY = window.innerHeight / 2;
+
+
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 function init() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 
-	group.add( text );
+	for (var i = 0, wordLength = settings.text.length; i < wordLength; ++i) {
+		var text3d = new THREE.TextGeometry( settings.text[i], {
+			size: 10,
+			height: 2,
+			curveSegments: 2,
+			font: "helvetiker"
+		});
+		var text = new THREE.Mesh(text3d, textMaterial);
+		group.add(text);
+		text.position.x = Math.random() * 500 - 250;
+		text.position.y = Math.random() * 200 - 100;
+		text.position.z = Math.random() * 500;
+	}
+
+	// group.add( text );
 	scene.add( group );
 
-	text.position.x = 0;
-	text.position.y = 0;
-	text.position.z = 0;
+	
 
 	camera.position.set( 0, 0, 500 );
 
 	render();
+	console.log(group);
 }
+
+function onDocumentMouseMove(event) {
+	mouseX = ( event.clientX - windowHalfX ) * 10;
+	mouseY = ( event.clientY - windowHalfY ) * 10;
+}
+
+function toScreenXY( position, camera, div ) {
+    var pos = position.clone();
+    projScreenMat = new THREE.Matrix4();
+    projScreenMat.multiply( camera.projectionMatrix, camera.matrixWorldInverse );
+    projScreenMat.multiplyVector3( pos );
+
+    var offset = findOffset(div);
+
+    return { x: ( pos.x + 1 ) * div.width / 2 + offset.left,
+         y: ( - pos.y + 1) * div.height / 2 + offset.top };
+
+}
+function findOffset(element) { 
+	var pos = new Object();
+	pos.left = pos.top = 0;        
+	if (element.offsetParent) { 
+		do { 
+		  pos.left += element.offsetLeft; 
+		  pos.top += element.offsetTop; 
+		} while (element = element.offsetParent); 
+	} 
+	return pos;
+} 
 
 function render() {
 	requestAnimationFrame(render);
+
+	for (var i = 0, groupLength = group.children.length; i < groupLength; ++i) {
+		group.children[i].position.x -= 1;
+		if (toScreenXY(group.children[i].position, camera, renderer.domElement).x < -1000) {
+			group.children[i].position.x = 100;
+		}
+	}
+	
+	camera.position.z = mouseY / 200 + 400;
+
+	//console.log(toScreenXY(group.children[0].position, camera, renderer.domElement));
+
 	renderer.render(scene, camera);
 }
 
