7e4ad3df3f2b5a90d2455d1f541acdfef4862107
Romain Braun <braun.rom@gmail.com>
2014-06-22 18:04:11 +0200

    2d version

---
+++ index2d.js
@@ -0,0 +1,45 @@
+var canvas,
+	fabricCanvas,
+	canvasW,
+	canvasH,
+	fontSize,
+	lineNumber = 15,
+	text = [];
+
+function init() {
+	canvas = document.getElementById("mainCanvas");
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
+    canvasW = canvas.width;
+    canvasH = canvas.height;
+
+    fontSize = Math.ceil(canvas.height / lineNumber);
+
+    fabricCanvas = new fabric.StaticCanvas('mainCanvas');
+for(var i = 0, textLength = 300; i < textLength; i++) {
+	var color = Math.floor(Math.random() * 150);
+	var phrase = new fabric.Text(settings.text[i], { fontSize: fontSize, fontFamily: 'alainblondelregular', fill: 'rgb('+color+','+color+','+color+')'});
+	phrase.top = i % lineNumber * (fontSize);
+	phrase.left = canvasW - Math.random() * (canvasW / 2);
+	if (text[i-15]) {
+		phrase.left = text[i-15].getBoundingRect().left + text[i-15].getBoundingRect().width;
+	} else {
+		phrase.left = canvasW - Math.random() * (canvasW / 2);
+	}
+	text.push(phrase);
+	fabricCanvas.add(phrase);
+}
+requestAnimationFrame(slideText);
+}
+// setTimeout(init, 1000);
+init();
+
+function slideText() {
+	var test = fabricCanvas.getObjects();
+	for (var i = 0; i < test.length; i++) {
+		test[i].set('left', test[i].left-3);
+	}
+	// text.set('left', text.left+1);
+	fabricCanvas.renderAll();
+	requestAnimationFrame(slideText);
+}
